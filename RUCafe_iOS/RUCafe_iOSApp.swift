c6ac233f7bb0052ed5761e258618248aaa41f45a
Kirill Vinogradov <61331094+Kirill-Vine@users.noreply.github.com>
2023-08-15 11:58:09 -0400

    Initial Commit

---
+++ RUCafe_iOS/RUCafe_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  RUCafe_iOSApp.swift
+//  RUCafe_iOS
+//
+//  Created by kv on 8/15/23.
+//
+
+import SwiftUI
+
+@main
+struct RUCafe_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
